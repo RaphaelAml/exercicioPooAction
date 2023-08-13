c10a579ccb8d19364c521d77a31abac9f99ebe7e
RaphaelAml <raphaelaml1@outlook.com>
2023-08-13 10:55:21 -0300

    Fazendo testa da classe

--- src/main/java/classes/Aluno.java
+++ src/main/java/classes/Aluno.java
@@ -1,2 +1,82 @@
-package classes;public class Aluno {
+package classes;
+
+public class Aluno {
+
+    private String nome;
+    private int idade;
+    private String dataNascimento;
+    private String numeroCpf;
+    private String dataMatricula;
+    private String nomeEscola;
+    private String serieMatriculado;
+
+    public Aluno() {
+
+
+    }
+
+    public Aluno(String nomePadrao){
+        nome = nomePadrao;
+    }
+
+    public Aluno(String nomePadrao, int idadePadrao){
+        nome =  nomePadrao;
+        idade = idadePadrao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getNumeroCpf() {
+        return numeroCpf;
+    }
+
+    public void setNumeroCpf(String numeroCpf) {
+        this.numeroCpf = numeroCpf;
+    }
+
+    public String getDataMatricula() {
+        return dataMatricula;
+    }
+
+    public void setDataMatricula(String dataMatricula) {
+        this.dataMatricula = dataMatricula;
+    }
+
+    public String getNomeEscola() {
+        return nomeEscola;
+    }
+
+    public void setNomeEscola(String nomeEscola) {
+        this.nomeEscola = nomeEscola;
+    }
+
+    public String getSerieMatriculado() {
+        return serieMatriculado;
+    }
+
+    public void setSerieMatriculado(String serieMatriculado) {
+        this.serieMatriculado = serieMatriculado;
+    }
 }
--- src/main/java/org/comandos/Main.java
+++ src/main/java/org/comandos/Main.java
@@ -1,7 +1,24 @@
-package org.example;
+package org.comandos;
+
+import classes.Aluno;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Aluno aluno1 = new Aluno();
+        Aluno aluno2 = new Aluno("Test");
+        Aluno aluno3 = new Aluno("Teste1",26);
+
+        aluno1.setNome("Teste");
+        aluno1.setDataMatricula("12");
+        aluno1.setIdade(26);
+        aluno1.setNomeEscola("Testando tudo");
+
+        System.out.println("Student name: " + aluno1.getNome());
+        System.out.println("Student id: " + aluno1.getDataMatricula());
+        System.out.println("Student age: " + aluno1.getIdade());
+        System.out.println("School: " + aluno1.getNomeEscola());
+
+
     }
 }
